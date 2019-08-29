9801a357fb2e23f95d061bc95d704f88dc10d1af
Steven Foong <foongws@ecsmy.com>
2019-08-29 17:28:40 +0800

    Update index.php

--- setup-db/index.php
+++ setup-db/index.php
@@ -5,7 +5,7 @@
    if(!$db) {
       echo $db->lastErrorMsg();
    } else {
-      echo "Opened database successfully\n";
+      echo "- Opened database successfully<br>";
    }
 
    // Create COMMENT Table
@@ -13,32 +13,19 @@
    $sql =<<<EOF
       CREATE TABLE COMMENT
       (TICKET_ID  	TEXT 	NOT NULL,
-      REPORTER           TEXT    NOT NULL,
-      ASSIGNEE           TEXT     NOT NULL,
+      REPORTER          TEXT    NOT NULL,
+      ASSIGNEE          TEXT    NOT NULL,
       ORGANIZARION	TEXT	NOT NULL,
       UPDATE_BY		TEXT	NOT NULL,
       COMMENT        	CHAR(50),
       UPDATE_FROM	TEXT	NOT NULL);
-   EOF;
+EOF;
 
    $ret = $db->exec($sql);
    if(!$ret){
       echo $db->lastErrorMsg();
    } else {
-      echo "COMMENT Table created successfully\n";
-   }
-
-   // Create INDEX table
-
-   $sql =<<<EOF
-	   CREATE UNIQUE INDEX idx_control_sessionid ON control (SESSION_ID);
-   EOF;
-
-   $ret = $db->exec($sql);
-   if(!$ret){
-      echo $db->lastErrorMsg();
-   } else {
-      echo "Index Table created successfully\n";
+      echo "- COMMENT Table created successfully<br>";
    }
 
    // Create CONTROL table
@@ -46,16 +33,32 @@
    $sql =<<<EOF
       CREATE TABLE CONTROL
       (ROW_ID_SENT  	TEXT 	NOT NULL,
+      SESSION_ID        TEXT    NOT NULL,
       LAST_SENT		TEXT	NOT NULL);
-   EOF;
+EOF;
 
    $ret = $db->exec($sql);
    if(!$ret){
       echo $db->lastErrorMsg();
    } else {
-      echo "CONTROL Table created successfully\n";
+      echo "- CONTROL Table created successfully<br>";
    }
+
+   // Create index of SESSION_ID column
+
+   $sql =<<<EOF
+	   CREATE UNIQUE INDEX idx_control_sessionid ON control (SESSION_ID);
+EOF;
+
+   $ret = $db->exec($sql);
+   if(!$ret){
+      echo $db->lastErrorMsg();
+   } else {
+      echo "- Index of SESSION_ID column created successfully<br>";
+   }
+
    $db->close();
 
-   echo "DB creation complete\n";
+   echo "- DB creation complete<br>";
+
 ?>
