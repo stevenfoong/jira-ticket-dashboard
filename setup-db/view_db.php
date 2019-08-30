b3808aa3f8ba6d5923102ba08c09a1752c5c3918
Steven Foong <foongws@ecsmy.com>
2019-08-30 17:33:00 +0800

    Update view_db.php

--- setup-db/view_db.php
+++ setup-db/view_db.php
@@ -6,12 +6,17 @@ $db = new MyDB();
 $result = $db->query('SELECT rowid, * FROM COMMENT');
 //var_dump($result->fetchArray());
 
+echo "From COMMENT:<br><br>";
+
 while ($row = $result->fetchArray()) {
     var_dump($row);
     echo '<br><br>';
 }
 
 $result2 = $db->query('SELECT rowid, * FROM CONTROL');
+
+echo "From CONTROL:<br><br>";
+
 while ($row2 = $result2->fetchArray()) {
     var_dump($row2);
     echo '<br><br>';
