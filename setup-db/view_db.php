e6900e6002a80e32d5cad8b411991ca6b966e658
Steven Foong <foongws@ecsmy.com>
2019-09-02 20:10:49 +0800

    Convert the array to table form

--- setup-db/view_db.php
+++ setup-db/view_db.php
@@ -7,20 +7,36 @@ $result = $db->query('SELECT rowid, * FROM COMMENT');
 //var_dump($result->fetchArray());
 
 echo "From COMMENT:<br><br>";
+echo '<table style="width:100%; border: 1px solid black" >';
 
 while ($row = $result->fetchArray()) {
-    var_dump($row);
-    echo '<br><br>';
+    //var_dump($row);
+    //echo '<br><br>';
+    echo '<tr style="border: 1px solid black">';
+    foreach($row as $value){
+      echo '<th style="border: 1px solid black">'.$value.'</th>';
+    }
+    echo "</tr>";
 }
+
+echo "</table>";
 
 $result2 = $db->query('SELECT rowid, * FROM CONTROL');
 
-echo "From CONTROL:<br><br>";
+echo "<br><br>From CONTROL:<br><br>";
+echo '<table style="width:100%; border: 1px solid black" >';
 
 while ($row2 = $result2->fetchArray()) {
-    var_dump($row2);
-    echo '<br><br>';
+    //var_dump($row2);
+    //echo '<br><br>';
+    echo '<tr style="border: 1px solid black">';
+    foreach($row2 as $value){
+      echo '<th style="border: 1px solid black">'.$value.'</th>';
+    }
+    echo "</tr>";
 }
+
+echo "</table>";
 
 $db->close();
 
